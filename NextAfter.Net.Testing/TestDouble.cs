1204eccb40c4b89f7cfb91a59f5c5030096a11cf
Dmitry <dimakro2010@yandex.ru>
2023-11-03 00:35:23 +0700

    update target framework for test project

--- NextAfter.Net.Testing/TestDouble.cs
+++ NextAfter.Net.Testing/TestDouble.cs
@@ -1,8 +1,6 @@
 ﻿// This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-using NUnit.Framework;
-
 namespace NextAfter.Net.Testing
 {
     [TestFixture]
